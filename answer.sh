1f8c40cf1c5661cd5ce598e60e0b7997283006e3
torakiri21216 <kiri21216@gmail.com>
2025-03-21 20:35:09 +0900

    second commit

--- answer.sh
+++ answer.sh
@@ -39,39 +39,39 @@ $ mkdir practice_dir_1
 $ ls -l
 
 # 10. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`というディレクトリを作成してください。**但し、相対パスを使用してください。**
-$ mkdir /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2
+$ mkdir ./practice_dir_1/practice_dir_2
 
 # 11. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`ディレクトリが作成されていることを確認(表示)してください。**但し、相対パスを使用してください。**
-$ ls /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1
+$ ls ./practice_dir_1
 practice_dir_2/
 
 # 12. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`./practice_dir_1/practice_dir_2`ディレクトリ内に`practice_file1.txt`、`practice_file2.txt`というファイルを**一回のコマンドで**作成してください。**但し、相対パスを使用してください。**
-$ touch /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2/practice_file1.txt /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2/practice_file2.txt
+$ touch ./practice_dir_1/practice_dir_2/practice_file1.txt ./practice_dir_1/practice_dir_2/practice_file2.txt
 
 # 13 `practice_dir_2`に移動してください。**但し、相対パスを使用してください。**
-$ cd /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2
+$ cd ./practice_dir_1/practice_dir_2
 
 # 14. `practice_file1.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**移動**してください。**但し、相対パスを使用してください。**
-$ mv /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2/practice_file1.txt /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice
+$ mv ./practice_file1.txt ../../LinuxCommandPractice
 
 # 15. `practice_file2.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**コピー**してください。**但し、相対パスを使用してください。**
-$ cp /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2/practice_file2.txt /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice
+$ cp ./practice_file2.txt ../../LinuxCommandPractice
 
 # 16. カレントディレクトリの`practice_file2.txt`を削除してください。**但し、相対パスを使用してください。**
-$ rm /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_file2.txt
+$ rm ./practice_file2.txt
 
 # 17. `practice_file2.txt`が削除されていることを確認(表示)してください。
-$ ls /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_file2.txt
-ls: cannot access '/c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_file2.txt': No such file or directory
+$ ls ./practice_file2.txt
+ls: cannot access './practice_file2.txt': No such file or directory
 # 18. 2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ移動してください。**但し、相対パスを使用してください。**
-$ cd /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice
+$ cd ../../
 
 # 19. `practice_dir_2`ディレクトリを削除してください。**但し、相対パスを使用してください。**
-$ rm -r /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2
+$ rm -r ./practice_dir_1/practice_dir_2
 
 # 20. `vi`または`vim`を用いて、カレントディレクトリにある`practice_file1.txt`に`I'm good at Linux commands!`と書き込んでください。
-$ vi /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_file1.txt
+$ vi ./practice_file1.txt
 
 # 21. `practice_file1.txt`に書き込んだ内容を出力してください。
-$ cat /c/Users/kiri2/geekationWorkspace/LinuxCommandPractice/practice_file1.txt
+$ cat ./practice_file1.txt
 I'm good at Linux commands!
